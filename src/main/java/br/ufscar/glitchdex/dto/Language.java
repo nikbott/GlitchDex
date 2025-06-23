d662f6b882f54c852668ab0c18fb9781c6ff8cb2
Vinicius Kury <viniciusreyeskury@protonmail.com>
2025-06-23 16:51:11 -0300

    small fixes
    Signed-off-by: Vinicius Kury <viniciusreyeskury@protonmail.com>

--- src/main/java/br/ufscar/glitchdex/dto/Language.java
+++ src/main/java/br/ufscar/glitchdex/dto/Language.java
@@ -1,37 +1,14 @@
 package br.ufscar.glitchdex.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Language {
     private String code;
     private String name;
     private String countryCode;
-
-    public Language(String code, String name, String countryCode) {
-        this.code = code;
-        this.name = name;
-        this.countryCode = countryCode;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountryCode() {
-        return countryCode;
-    }
-
-    public void setCountryCode(String countryCode) {
-        this.countryCode = countryCode;
-    }
 }
